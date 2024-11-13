d8a81c6e37fc380ca5aced03c7fbba71cd0def87
yahyasamet <73041526+yahyasamet@users.noreply.github.com>
2024-11-13 20:27:25 +0100

    cross

--- DevOps_Project/src/main/java/tn/esprit/devops_project/config/CorsConfig.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/config/CorsConfig.java
@@ -1,12 +1,11 @@
-package tn.esprit.devops_project.controllers;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class WebConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +14,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("http://192.168.50.4")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
