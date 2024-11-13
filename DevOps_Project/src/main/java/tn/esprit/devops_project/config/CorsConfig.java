0a81be5d463e5e00290f957a06215bb031a658ba
yahyasamet <73041526+yahyasamet@users.noreply.github.com>
2024-11-13 20:00:23 +0100

    CrosConfig

---
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package tn.esprit.devops_project.controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://192.168.50.4")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
